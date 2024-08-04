94be0ab65507556cb287d533b65d55ae3c69ee44
Albaash247 <albaash247@gmail.com>
2024-08-04 18:01:03 +0400

    rename api file

--- js/constants/api.js
+++ js/constants/api.js
@@ -9,6 +9,11 @@ export const API_URL_REGISTER = `${selectedEnv}/api/kite/register`;
 export const API_URL_PROJECTS = `${selectedEnv}/api/kite/projects`;
 export const API_URL_TASKS = `${selectedEnv}/api/kite/tasks`;
 
+export const API_ERROR_MSG_ALL_FIELDS_MANDATORY = "All fields are mandatory.";
+export const API_ERROR_MSG_WRONG_CREDENTIALS = "wrong credentials";
+
+
+
 export const HTTP_METHOD_GET_NO_CACHE = {
     method: 'GET', headers: {
         'Content-Type': 'application/json',
