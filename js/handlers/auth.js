9dd91b50a504dfe3e4b2cf545e28a0fbeee5641a
Albaash247 <albaash247@gmail.com>
2024-08-04 13:11:00 +0400

    login function

--- js/handlers/auth.js
+++ js/handlers/auth.js
@@ -1,5 +1,6 @@
 import store from 'https://cdn.jsdelivr.net/npm/store@2.0.12/+esm'
 import { STORE_USER } from '../constants/store-keys.js';
+import { API_URL_LOGIN, HTTP_METHOD_POST_NO_CACHE } from '../constants/api-urls.js';
 
 export function storeUser(user) {
     store.set(STORE_USER, user)
@@ -7,4 +8,19 @@ export function storeUser(user) {
 
 export function getUser() {
     return store.get(STORE_USER);
+}
+
+export async function fetchLogin(user) {
+    try {
+        const response = await fetch(API_URL_LOGIN, HTTP_METHOD_POST_NO_CACHE(user));
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status} | ${response.message}`);
+        }
+        const responseObject = await response.json();
+        storeUser(responseObject.data);
+        console.log(responseObject);
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
