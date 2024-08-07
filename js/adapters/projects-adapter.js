587dfaf01a5efb12169d51f23b239a86a2ddcbe4
AlineJo <albaash247>
2024-08-08 00:40:12 +0400

    display projects working

--- js/adapters/projects-adapter.js
+++ js/adapters/projects-adapter.js
@@ -1,36 +1,27 @@
 
-const taskRowTemplate = document.querySelector('#taskRowTemplate');
-const contentContainer = document.querySelector('#contentContainer');
+const projectListItemTemplate = document.querySelector('#projectListItemTemplate');
+const projectsContainer = document.querySelector('#projectsContainer');
 
 export function adapter(projects) {
-    projects.forEach(project => {
-        console.log(project);
-        //  createRow(projects);
+
+    projects.forEach((project, index) => {
+        createListItem(project, index);
     });
 }
 
-function createRow(task) {
+function createListItem(project, index) {
 
-    const clone = taskRowTemplate.content.cloneNode(true);
+    const clone = projectListItemTemplate.content.cloneNode(true);
 
-    const task_id = clone.querySelector('#task_id');
-    const task_title = clone.querySelector('#task_title');
-    const task_author_name = clone.querySelector('#task_author_name');
-    const task_project_name = clone.querySelector('#task_project_name');
-    const task_status = clone.querySelector('#task_status');
-    const task_importance = clone.querySelector('#task_importance');
-    const task_due_date = clone.querySelector('#task_due_date');
-    const task_content = clone.querySelector('#task_content');
+    const listItem = clone.querySelector('#listItem');
+    const linkElement = clone.querySelector('#linkElement');
+    const projectTitle = clone.querySelector('#projectTitle');
 
-    task_id.innerText = task.id;
-    task_title.innerText = task.title;
-    task_author_name.innerText = task.author_name;
-    task_project_name.innerText = task.project_name;
-    task_status.innerText = task.status;
-    task_importance.innerText = task.importance;
-    task_due_date.innerText = task.due_date;
-    task_content.innerText = task.content;
+    listItem.dataset.project_id = project.id;
+    projectTitle.innerText = project.project_name;
 
-    contentContainer.appendChild(clone);
+    index == 0 ? linkElement.classList.add("active") : null;
+
+    projectsContainer.appendChild(clone);
 
 }
--- js/handlers/kite-projects.js
+++ js/handlers/kite-projects.js
@@ -4,11 +4,17 @@ import { adapter } from "../adapters/projects-adapter.js";
 
 export async function fetchProjects() {
     try {
-        const response = await fetch(API_URL_PROJECTS, HTTP_METHOD_POST_NO_CACHE(getUser().id));
+        console.log(getUser().id);
+
+        var jsonRequestBody= {
+            "id" : getUser().id,
+        }
+        
+        const response = await fetch(API_URL_PROJECTS, HTTP_METHOD_POST_NO_CACHE(jsonRequestBody));
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const responseObject = await response.json();
+        const responseObject = await response.json();   
         adapter(responseObject.data.projects);
     } catch (error) {
         console.error('Error fetching data:', error);
