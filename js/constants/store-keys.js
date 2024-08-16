e3840219c32743a4275f8b9969888f921a32848e
Albaash247 <albaash247@gmail.com>
2024-08-16 22:44:14 +0400

    # store selected project id, move store and get user to the file

--- js/constants/store-keys.js
+++ js/constants/store-keys.js
@@ -1,1 +1,22 @@
+import store from 'https://cdn.jsdelivr.net/npm/store@2.0.12/+esm'
+
 export const STORE_USER = 'user';
+export const STORE_SELECTED_PROJECT_ID = 'selected_project';
+
+
+
+export function storeUser(user) {
+    store.set(STORE_USER, user);
+}
+
+export function getUser() {
+    return store.get(STORE_USER);
+}
+
+export function storeSelectedProjectId(projectId) {
+    store.set(STORE_SELECTED_PROJECT_ID, projectId);
+}
+
+export function getSelectedProjectId() {
+    return store.get(STORE_SELECTED_PROJECT_ID);
+}
