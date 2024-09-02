1b7e243b611da958e432961459eaf2c022d17740
Albaash247 <albaash247@gmail.com>
2024-09-02 11:11:31 +0400

    # auto display new project modal for users with no projects

--- js/fetching/kite-projects.js
+++ js/fetching/kite-projects.js
@@ -1,9 +1,13 @@
 import { mainFetch } from '../fetching/main-fetch.js';
 import { API_URL_GET_PROJECTS, API_URL_STORE_PROJECT } from "../constants/api.js";
 import { adapter } from "../adapters/projects-adapter.js";
+import { displayProjectModal } from "../scripts/new-project.js";
 
 export async function fetchProjects(jsonRequestBody) {
     const responseObject = await mainFetch(API_URL_GET_PROJECTS, jsonRequestBody);
+    if (responseObject.data.projects.length == 0) {
+        displayProjectModal();
+    }
     adapter(responseObject.data.projects);
 }
 
